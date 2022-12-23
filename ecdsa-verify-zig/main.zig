e80d9d1262eeaf65770d3e03c321989bf52b3f20
Joel Jakobsson <joel@compiler.org>
2022-12-23 23:10:53 +0100

    Zig code improvements by jeltz
    https://gist.github.com/jeltz/54fcdb8f84b4ce7815cde8ed40f85b6a

--- ecdsa-verify-zig/main.zig
+++ ecdsa-verify-zig/main.zig
@@ -2,48 +2,53 @@ const std = @import("std");
 const bigint = std.math.big.int.Managed;
 const os = std.os;
 const testing = std.testing;
+const Allocator = std.mem.Allocator;
 
-fn modular_inverse(a: *bigint, n: *bigint) !bigint {
-    var zero = try bigint.initSet(std.heap.c_allocator, 0);
-    var one = try bigint.initSet(std.heap.c_allocator, 1);
+fn modular_inverse(allocator: Allocator, a: *bigint, n: *bigint) !bigint {
+    var zero = try bigint.initSet(allocator, 0);
     defer zero.deinit();
+    var one = try bigint.initSet(allocator, 1);
     defer one.deinit();
+
     if (n.eq(zero) or n.eq(one)) {
         return error.NoModularInverse;
     }
-    var t = try bigint.initSet(std.heap.c_allocator, 0);
-    var newt = try bigint.initSet(std.heap.c_allocator, 1);
-    var r = try bigint.initSet(std.heap.c_allocator, 0);
-    var newr = try bigint.initSet(std.heap.c_allocator, 0);
+
+    var t = try bigint.initSet(allocator, 0);
+    errdefer t.deinit();
+    var newt = try bigint.initSet(allocator, 1);
     defer newt.deinit();
+    var r = try bigint.init(allocator);
     defer r.deinit();
+    var newr = try bigint.init(allocator);
     defer newr.deinit();
+
     try r.copy(n.toConst());
     try newr.copy(a.toConst());
-    var quotient = try bigint.initSet(std.heap.c_allocator, 0);
-    var remainder = try bigint.initSet(std.heap.c_allocator, 0);
-    var tmp1 = try bigint.initSet(std.heap.c_allocator, 0);
-    var tmp2 = try bigint.initSet(std.heap.c_allocator, 0);
-    var tmp3 = try bigint.initSet(std.heap.c_allocator, 0);
-    defer tmp1.deinit();
-    defer tmp2.deinit();
-    defer tmp3.deinit();
+
+    var quotient = try bigint.init(allocator);
+    defer quotient.deinit();
+    var remainder = try bigint.init(allocator);
+    defer remainder.deinit();
+    var tmp = try bigint.init(allocator);
+    defer tmp.deinit();
+
     while (!newr.eqZero()) {
-        try bigint.divFloor(&quotient, &remainder, &r, &newr);
-        try tmp1.copy(t.toConst());
-        try t.copy(newt.toConst());
-        try bigint.mul(&tmp3, &quotient, &newt);
-        try newt.sub(&tmp1, &tmp3);
-        try tmp2.copy(r.toConst());
-        try r.copy(newr.toConst());
-        try bigint.mul(&tmp3, &quotient, &newr);
-        try newr.sub(&tmp2, &tmp3);
+        try quotient.divFloor(&remainder, &r, &newr);
+
+        try tmp.mul(&quotient, &newt);
+        t.swap(&newt);
+        try newt.sub(&newt, &tmp);
+
+        try tmp.mul(&quotient, &newr);
+        r.swap(&newr);
+        try newr.sub(&newr, &tmp);
     }
     if (r.order(one) == .gt) {
         return error.NoModularInverse;
     }
     if (t.order(zero) == .lt) {
-        try bigint.add(&t, &t, n);
+        try t.add(&t, n);
     }
     return t;
 }
@@ -55,35 +60,35 @@ pub fn main() !void {
     }
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     var a = try bigint.init(std.heap.page_allocator);
-    var n = try bigint.init(std.heap.page_allocator);
     defer a.deinit();
+    var n = try bigint.init(std.heap.page_allocator);
     defer n.deinit();
     try a.setString(16, args[1]);
     try n.setString(16, args[2]);
     var result = try bigint.init(std.heap.page_allocator);
-    result = try modular_inverse(&a, &n);
+    result = try modular_inverse(std.heap.page_allocator, &a, &n);
     std.debug.print("{x}\n", .{result});
 }
 
 test "modular_inverse" {
     var a = try bigint.initSet(std.heap.page_allocator, 0x69c6e9f8d9bcbdba4e5478cb75b084332d51b0be2c21701b157c7c87abb98057);
-    var n = try bigint.initSet(std.heap.page_allocator, 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141);
     defer a.deinit();
+    var n = try bigint.initSet(std.heap.page_allocator, 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141);
     defer n.deinit();
     var expected_result = try bigint.initSet(std.heap.page_allocator, 0x321e5faa4b27bb008db179136e9c4ecf781542f425541ea27ecdf6236cb776b0);
     defer expected_result.deinit();
-    var result = try modular_inverse(&a, &n);
+    var result = try modular_inverse(std.heap.page_allocator, &a, &n);
     try testing.expect(result.eq(expected_result));
 }
 
 test "modular_inverse_no_inverse" {
     var a = try bigint.initSet(std.heap.page_allocator, 4);
-    var n = try bigint.initSet(std.heap.page_allocator, 6);
     defer a.deinit();
+    var n = try bigint.initSet(std.heap.page_allocator, 6);
     defer n.deinit();
-    try testing.expectError(error.NoModularInverse, modular_inverse(&a, &n));
+    try testing.expectError(error.NoModularInverse, modular_inverse(std.heap.page_allocator, &a, &n));
     try n.set(0);
-    try testing.expectError(error.NoModularInverse, modular_inverse(&a, &n));
+    try testing.expectError(error.NoModularInverse, modular_inverse(std.heap.page_allocator, &a, &n));
     try n.set(1);
-    try testing.expectError(error.NoModularInverse, modular_inverse(&a, &n));
+    try testing.expectError(error.NoModularInverse, modular_inverse(std.heap.page_allocator, &a, &n));
 }
